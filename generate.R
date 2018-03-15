8b7183663dcd2ca5d9e17f31e4f9e2fb34b0b112
allthesignals <wilburnforce@gmail.com>
2018-03-15 12:23:37 -0400

    Working data munging

--- generate.R
+++ generate.R
@@ -2,7 +2,22 @@ library('tidyverse')
 library('sf')
 library('lwgeom')
 
-tracts <- st_read("data/tracts/RegionalLabs_CensusTracts_withData.shp")
-dots <- st_sample(tracts, tracts$EmPr15 / 100) %>% 
+per_dot <- 100
+
+# read in the data, select only what's needed, and divide variables
+# by the per_dot factor. Include a total of the new per_dot.
+tracts <- st_read("data/tracts/RegionalLabs_CensusTracts_withData.shp") %>%
+  select(c(geometry, EmOff15, EmIns15, EmInd15, EmSer15, EmOth15)) %>%
+  mutate(
+    EmOff15 = EmOff15 / per_dot,
+    EmIns15 = EmIns15 / per_dot,
+    EmInd15 = EmInd15 / per_dot,
+    EmSer15 = EmSer15 / per_dot,
+    EmOth15 = EmOth15 / per_dot,
+    total = EmOff15 + EmIns15 + EmInd15 + EmSer15 + EmOth15
+  )
+
+# randomly distribute dots from the tracts and join back in
+dots <- st_sample(tracts, tracts$total) %>% 
   st_sf %>% 
   st_join(tracts)
