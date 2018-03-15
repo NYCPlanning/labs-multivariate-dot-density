7b81c1b9047ba354fe254695a19519b65b26056e
allthesignals <wilburnforce@gmail.com>
2018-03-15 14:50:22 -0400

    Working script

--- generate.R
+++ generate.R
@@ -9,11 +9,11 @@ per_dot <- 100
 tracts <- st_read("data/tracts/RegionalLabs_CensusTracts_withData.shp") %>%
   select(c(geometry, EmOff15, EmIns15, EmInd15, EmSer15, EmOth15)) %>%
   mutate(
-    EmOff15 = EmOff15 / per_dot,
-    EmIns15 = EmIns15 / per_dot,
-    EmInd15 = EmInd15 / per_dot,
-    EmSer15 = EmSer15 / per_dot,
-    EmOth15 = EmOth15 / per_dot,
+    EmOff15 = as.integer(EmOff15 / per_dot),
+    EmIns15 = as.integer(EmIns15 / per_dot),
+    EmInd15 = as.integer(EmInd15 / per_dot),
+    EmSer15 = as.integer(EmSer15 / per_dot),
+    EmOth15 = as.integer(EmOth15 / per_dot),
     total = EmOff15 + EmIns15 + EmInd15 + EmSer15 + EmOth15
   )
 
@@ -21,3 +21,21 @@ tracts <- st_read("data/tracts/RegionalLabs_CensusTracts_withData.shp") %>%
 dots <- st_sample(tracts, tracts$total) %>% 
   st_sf %>% 
   st_join(tracts)
+
+dots <- dots %>%
+  rowwise %>%
+  mutate(
+    category = list(
+      c(
+        rep('EmOff15', EmOff15), 
+        rep('EmIns15', EmIns15),
+        rep('EmInd15', EmInd15),
+        rep('EmSer15', EmSer15),
+        rep('EmOth15', EmOth15)
+      )
+    )
+  ) %>%
+  rowwise %>%
+  mutate(
+    category = sample(category, 1)
+  )
